c88859fc1384a178daf10eda928891ac442584f2
ShellsOfSilver <shellsofsilver13@gmail.com>
2017-09-11 20:38:51 +0300

    fy

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l(yq$p@@7lz-m6r)*dvzv6v$u15c9ih_uovgjiwjdk&_fif#j('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
